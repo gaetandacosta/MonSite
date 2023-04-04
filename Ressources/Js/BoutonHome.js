3bd6983c7eb2ea292ae565bd998a85b7c31b4d22
Gaetan DaCosta <gaetan.da-costa@etu.umontpellier.fr>
2023-04-04 09:05:52 +0200

    commencement carrousel

--- Ressources/Js/BoutonHome.js
+++ Ressources/Js/BoutonHome.js
@@ -2,12 +2,13 @@ let btn = document.getElementById("btn-home")
 
 let image = document.querySelector("img")
 
-btn.addEventListener("mouseenter", function (){
-    image.src = "Ressources/images/yellow-arrow-up.png"
-})
-image.addEventListener("mouseenter", function (){
+btn.addEventListener("mouseover", function (){
     image.src = "Ressources/images/yellow-arrow-up.png"
 })
 btn.addEventListener("mouseout", function (){
     image.src = "Ressources/images/black-arrow-up.png"
+})
+
+btn.addEventListener("click", function () {
+    document.location.href="#main";
 })
