4910cc4ac7415a93e1fdfede18c9cac36372f2ba
Gaetan DaCosta <gaetan.da-costa@etu.umontpellier.fr>
2023-04-06 15:36:03 +0200

    finition carrousel projet

--- Ressources/Js/BoutonHome.js
+++ Ressources/Js/BoutonHome.js
@@ -3,10 +3,10 @@ let btn = document.getElementById("btn-home")
 let image = document.querySelector("img")
 
 btn.addEventListener("mouseover", function (){
-    image.src = "Ressources/images/yellow-arrow-up.png"
+    image.src = "https://cdn.discordapp.com/attachments/782961560061476894/1093527655078449213/yellow-arrow-up.png"
 })
 btn.addEventListener("mouseout", function (){
-    image.src = "Ressources/images/black-arrow-up.png"
+    image.src = "https://cdn.discordapp.com/attachments/782961560061476894/1093527644345225346/black-arrow-up.png"
 })
 
 btn.addEventListener("click", function () {
