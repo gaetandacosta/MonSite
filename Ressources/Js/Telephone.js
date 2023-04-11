32a718e97405a92486a93ee8a106ec70a1a876c2
dacostag <gaetan.da-costa@etu.umontpellier.fr>
2023-04-11 20:02:16 +0200

    finis

---
+++ Ressources/Js/Telephone.js
@@ -0,0 +1,17 @@
+
+let bouton = document.getElementById("copy")
+
+bouton.addEventListener('click', function (){
+
+
+
+
+
+    navigator.clipboard.writeText("0614856139")
+
+
+
+    alert("Vous avez copié mon numéro : " + "0614856139");
+})
+
+
