deb7b34ada5c2aa26abc7109e09f959db96d108b
Gaetan Da Costa <gaetan84290@gmail.com>
2023-11-11 22:07:09 +0100

    modification css

--- Ressources/Js/Telephone.js
+++ Ressources/Js/Telephone.js
@@ -3,14 +3,7 @@ let bouton = document.getElementById("copy")
 
 bouton.addEventListener('click', function (){
 
-
-
-
-
     navigator.clipboard.writeText("0614856139")
-
-
-
     alert("Vous avez copié mon numéro : " + "0614856139");
 })
 
