e5f8b9dd867c67d6dd8bec46884d773ad819bd78
Gaetan Da Costa <gaetan84290@gmail.com>
2023-11-12 13:34:13 +0100

    Update BarreLecture.js

--- Ressources/Js/BarreLecture.js
+++ Ressources/Js/BarreLecture.js
@@ -11,7 +11,7 @@ let scroll = () => {
     const percentage = Math.floor((position / trackLenght) * 100);
     
 
-    barre_lecture.style.right = 85 - percentage + "%"
+    barre_lecture.style.right = 90 - percentage + "%"
 }
 
 window.addEventListener('scroll', scroll);
