7e2dcd182577ce1bbdbc7dc6315a216c9b5a50de
Gaetan Da Costa <gaetan84290@gmail.com>
2023-11-12 13:42:57 +0100

    réparations

--- Ressources/Js/BoutonHome.js
+++ Ressources/Js/BoutonHome.js
@@ -1,14 +1,5 @@
 let btn = document.getElementById("btn-home")
 
-let image = document.querySelector("img")
-
-btn.addEventListener("mouseover", function (){
-    image.src = "https://cdn.discordapp.com/attachments/782961560061476894/1093527655078449213/yellow-arrow-up.png"
-})
-btn.addEventListener("mouseout", function (){
-    image.src = "https://cdn.discordapp.com/attachments/782961560061476894/1093527644345225346/black-arrow-up.png"
-})
-
 btn.addEventListener("click", function () {
     document.location.href="#main";
 })
