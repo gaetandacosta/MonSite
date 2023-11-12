b12d3037b7329cc93a05cf96c28f1ca14c1cc001
Gaetan Da Costa <gaetan84290@gmail.com>
2023-11-12 12:34:36 +0100

    barre lecture

---
+++ Ressources/Js/BarreLecture.js
@@ -0,0 +1,18 @@
+const barre_lecture = document.getElementById('barre_lecture');
+barre_lecture.style.display ="none";
+
+let scroll = () => {
+    barre_lecture.style.display = "block";
+    const height = document.body.scrollHeight;
+    const windowHeight = window.innerHeight;
+    const position = window.pageYOffset;
+
+    const trackLenght = height - windowHeight;
+    const percentage = Math.floor((position / trackLenght) * 100);
+
+    barre_lecture.style.right = 100 - percentage + "%"
+}
+
+window.addEventListener('scroll', scroll);
+
+
