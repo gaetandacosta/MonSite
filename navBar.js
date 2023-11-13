669555c3709a454afbe633fcc1e730c6f3d218f6
Gaetan Da Costa <gaetan84290@gmail.com>
2023-11-13 21:37:17 +0100

    Update navBar.js

--- navBar.js
+++ navBar.js
@@ -5,7 +5,8 @@ let listHoverText = bar.querySelectorAll('.hover-text');
 
 for (let index = 0; index < listBouton.length; index++) {
     listSousMenu[index].addEventListener('mouseover', () => {
-        listHoverText[index].style.color = 'green';
+        listHoverText[index].style.width= '100%';
+        listHoverText[index].style.filter= 'drop-shadow(0 0 23px #F5CB5C)';
         console.log('Déclenché');
     })
 
