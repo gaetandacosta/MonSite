bca920abfcd99f48ae5d1921431ea5a5b88d837f
Gaetan Da Costa <gaetan84290@gmail.com>
2023-11-13 21:31:08 +0100

    maj js

---
+++ navBar.js
@@ -0,0 +1,11 @@
+let bar = document.getElementById('navBar');
+let listBouton = bar.querySelectorAll('a');
+let listSousMenu = bar.querySelectorAll('.sous-menu');
+let listHoverText = bar.querySelectorAll('.hover-text');
+
+for (let index = 0; index < listBouton.length; index++) {
+    listSousMenu[index].addEventListener('mouseover', () => {
+        listHoverText[index].style.color = 'red';
+    })
+
+}
