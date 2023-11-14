78f879af9246b6241729b647c2ff5d2fc5d5a7a7
Gaetan Da Costa <gaetan84290@gmail.com>
2023-11-14 20:26:06 +0100

    test nav bar

--- Ressources/Js/navBar.js
+++ Ressources/Js/navBar.js
@@ -1,4 +1,6 @@
 let bar = document.getElementById('navBar');
+let listButtonBar = bar.querySelectorAll('.button-navBar');
+let listText = bar.querySelectorAll('.actual-text');
 let listSousMenu = bar.querySelectorAll('.sous-menu');
 let listHoverText = bar.querySelectorAll('.hover-text');
 
@@ -7,12 +9,22 @@ for (let index = 0; index < listSousMenu.length; index++) {
     .addEventListener('mouseover', () => {
         listHoverText[index].classList.add('hover-js');
         listSousMenu[index].style.zIndex = "9";
+        listText[index].classList.add('black-text');
     })
     listSousMenu[index]
     .addEventListener('mouseleave', () => {
         listHoverText[index].classList.remove('hover-js')
         listSousMenu[index].style.zIndex = "7";
+        listText[index].classList.remove('black-text');
     })
-
+    
+    listButtonBar[index]
+    .addEventListener('mouseover', () => {
+        listText[index].classList.add('black-text');
+    })
+    listSousMenu[index]
+    .addEventListener('mouseleave', () => {
+        listText[index].classList.remove('black-text');
+    })
 
 }
