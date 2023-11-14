960608e525544b9871c04e926c31eb2fd13ba2ef
Gaetan Da Costa <gaetan84290@gmail.com>
2023-11-14 21:59:17 +0100

    nouvelle page d'accueil

--- Ressources/Js/Telephone.js
+++ Ressources/Js/Telephone.js
@@ -3,8 +3,14 @@ let bouton = document.getElementById("copy")
 
 bouton.addEventListener('click', function (){
 
-    navigator.clipboard.writeText("0614856139")
-    alert("Vous avez copié mon numéro : " + "0614856139");
+    navigator.clipboard.writeText("0614856139").then(() => {
+        alert("Vous avez copié mon numéro : " + "0614856139");
+      },() => {
+        alert("Voici mon numéro : " + "0614856139");
+      });
+
+    
+   
 })
 
 
